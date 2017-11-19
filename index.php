459d44374bbf0e60d925a706c09abcc4101aba2d
Achmad Zulkarnain <ain.zizou@gmail.com>
2017-11-20 00:22:33 +0800

    Navbar Fixed
    Fixing navigation bar display

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once('includes/connect.php');
 include('header.php');
+#include('navbar.php');
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -18,14 +19,25 @@ if (!$result) {
   exit;
 }
 
+?>
 
+<table bgcolor="#FFFFFF" width="200" border="0">
+<tr><thead><td>No</td>
+<td>Name</td>
+<td>Address</td></thead></tr>
+
+<?php
 
 while ($row = pg_fetch_row($result)) {
-  echo "No: $row[0]  Name: $row[1]  Addr: $row[2] <br>";
+	echo "<tr><td>$row[0]</td>";
+	echo "<td>$row[1]</td>";
+	echo "<td>$row[2]</td></tr>";
+  #echo "No: $row[0]  Name: $row[1]  Addr: $row[2] <br>";
   
 }
-
 ?>
+
+</table>
 <body>
 </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -2,25 +2,16 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
 <link href="includes/style.css" rel="stylesheet" type="text/css" />
 </head>
-<div id="header">
-<div align="left">
+
 <body>
-<table style="background-color:#6CF" width="200" border="0">
-  <tr>
-    <td width="50%" height="115" style="text-align:left"><img src="images/gst.png" width="214" height="57" /></td>
-    <td width="50%" style="text-align:right"><img src="images/logo.png" width="275" height="113" /></td>
-  </tr>
-</table>
 <ul class="topnav">
   <li><a class="active" href="#home">Home</a></li>
   <li><a href="#news">News</a></li>
   <li><a href="#contact">Contact</a></li>
-  <li style="float:right"><a href="#about">Logout</a></li>
+  <li class="right"><a href="#about">About</a></li>
 </ul>
 </body>
-</div>
-</div>
-
 </html>
